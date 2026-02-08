93c050c87a7170f9d27b79f1aa31da972364849e
Franaffel <franksbass19@gmail.com>
2026-02-08 16:41:07 +0800

    small fix on exports

--- adapto/Lessons/lesson.gd
+++ adapto/Lessons/lesson.gd
@@ -1,8 +1,15 @@
 extends Resource
 class_name Lesson
 
-var lesson_title = ""
-var lesson_items = Array[LessonItem]
+@export var lesson_title = ""
+@export var lesson_items: Array[LessonItem]
 
-func get_random_lesson_item():
-	pass
+func get_random_lesson_item() -> LessonItem:
+	if lesson_items.is_empty():
+		return null
+	return lesson_items.pick_random()
+
+func get_items_by_difficulty(target_difficulty: int) -> Array[LessonItem]:
+	return lesson_items.filter(func(item):
+		return item.difficulty == target_difficulty
+	)
--- adapto/Lessons/lesson_item.gd
+++ adapto/Lessons/lesson_item.gd
@@ -8,7 +8,7 @@ class_name LessonItem
 @export var definition = "the pillar that hides data, preventing users from accessing it"
 @export var simple_terms = "public = access, private = no access"
 @export var examples = ["public", "private"]
-@export var image = Texture2D
+@export var image: Texture2D
 @export var difficulty = 2
 @export var related_to = ["pillars"]
 @export var type_of_information = ["list", "defined", "definition", "explain", "apply"] #more or less
