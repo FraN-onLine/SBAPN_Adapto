95881d1a11773e04d0eabc51f6473a5a22cf36ab
Franaffel <franksbass19@gmail.com>
2026-03-02 08:35:06 +0800

    mm

--- adapto/Menus/main_menu.gd
+++ adapto/Menus/main_menu.gd
@@ -7,7 +7,26 @@ func _on_start_button_pressed() -> void:
 	$Control/VBoxContainer/Button3.visible = false
 	$Control/VBoxContainer/Button4.visible = true
 	$Control/VBoxContainer/Button5.visible = true
-	$Control/VBoxContainer/Button6.visible = true
+	$Control/VBoxContainer/Back.visible = true
 
 func on_diagnostic_button_pressed():
 	get_tree().change_scene_to_file("res://Games/game1.tscn")
+
+
+func _on_button_2_pressed() -> void:
+	$Control/VBoxContainer/Button.visible = false
+	$Control/VBoxContainer/Button2.visible = false
+	$Control/VBoxContainer/Button3.visible = false
+	$Control/VBoxContainer/TopicSelect.visible = true
+	$Control/VBoxContainer/TopicImport.visible = true
+	$Control/VBoxContainer/Back.visible = true
+	
+func _on_back_pressed() -> void:
+	$Control/VBoxContainer/Button.visible = true
+	$Control/VBoxContainer/Button2.visible = true
+	$Control/VBoxContainer/Button3.visible = true
+	$Control/VBoxContainer/Button4.visible = false
+	$Control/VBoxContainer/Button5.visible = false
+	$Control/VBoxContainer/TopicSelect.visible = false
+	$Control/VBoxContainer/TopicImport.visible = false
+	$Control/VBoxContainer/Back.visible = false
